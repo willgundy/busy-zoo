c1f2b5b9cbae1360391c2a24443cd369a7e377cf
Will Gundy <willgunderson503@gmail.com>
2022-05-19 09:39:29 -0700

    added tests for all parade buttons

--- src/App.test.js
+++ src/App.test.js
@@ -26,4 +26,36 @@ test('make sure each button in the parade section renders the correct animal emo
   fireEvent.click(elephantButton);
   const elephantCountAfter = screen.queryAllByText(/🐘/i);
   expect(elephantCountAfter.length).toBe(2);
+
+  //tiger count, which should start at one and go to two after click event
+  const tigerCountBefore = screen.queryAllByText(/🐅/i);
+  expect(tigerCountBefore.length).toBe(1);
+  const tigerButton = screen.getByText(/Add Tiger/i);
+  fireEvent.click(tigerButton);
+  const tigerCountAfter = screen.queryAllByText(/🐅/i);
+  expect(tigerCountAfter.length).toBe(2);
+
+  //giraffe count, which should start at zero and go to one after click event
+  const giraffeCountBefore = screen.queryAllByText(/🦒/i);
+  expect(giraffeCountBefore.length).toBe(0);
+  const giraffeButton = screen.getByText(/Add Giraffe/i);
+  fireEvent.click(giraffeButton);
+  const giraffeCountAfter = screen.queryAllByText(/🦒/i);
+  expect(giraffeCountAfter.length).toBe(1);
+
+  //rhino count, which should start at one and go to two after click event
+  const rhineBeforecount = screen.queryAllByText(/🦏/i);
+  expect(rhineBeforecount.length).toBe(1);
+  const rhinoButton = screen.getByText(/Add Rhino/i);
+  fireEvent.click(rhinoButton);
+  const rhinoCountAfter = screen.queryAllByText(/🦏/i);
+  expect(rhinoCountAfter.length).toBe(2);
+
+  //gorilla count, which should start at zero and go to one after click event
+  const gorillaCountBefore = screen.queryAllByText(/🦍/i);
+  expect(gorillaCountBefore.length).toBe(0);
+  const gorillaButton = screen.getByText(/Add Gorilla/i);
+  fireEvent.click(gorillaButton);
+  const gorillaCountAfter = screen.queryAllByText(/🦍/i);
+  expect(gorillaCountAfter.length).toBe(1);
 });
