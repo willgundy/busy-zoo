941c8b976b01102ffe6e182be1903b458353efa8
Will Gundy <willgunderson503@gmail.com>
2022-05-19 09:25:22 -0700

    added first test of open/close sign

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import App from './App';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('when open button is click text changes to Busy Zoo is Open, when close button is clicked text changes to Sorry we are Closed', () => {
+  render(<App />);
+  const zooClosedButton = screen.queryByText(/Close the Zoo!/i);
+  const zooOpenButton = screen.queryByText(/Open the Zoo!/i);
+
+  fireEvent.click(zooOpenButton);
+
+  const zooOpenEl = screen.queryByText('Busy Zoo is Open!');
+  expect(zooOpenEl).toBeInTheDocument();
+
+  fireEvent.click(zooClosedButton);
+
+  const zooClosedEl = screen.queryByText('Sorry We are Closed!');
+  expect(zooClosedEl).toBeInTheDocument();
 });
