23398d000b1e2f54a92f479f3470af59fc05b977
Will Gundy <willgunderson503@gmail.com>
2022-05-19 12:42:34 -0700

    added my table, switching to munge assignment before coming back

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { Route, Switch, NavLink, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Fight from './Fight';
 import OpenSign from './OpenSign';
 import Parade from './Parade';
+import AdminPage from './AdminPage';
 
 function App() {
   const [animalOneSize, setAnimalOneSize] = useState(10);
@@ -11,27 +13,39 @@ function App() {
   const [animals, setAnimals] = useState(['elephant', 'tiger', 'rhino']);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        Welcome to Busy Zoo!
-      </header>
-      <main>
-        <Fight 
-          animalOneSize={animalOneSize}
-          animalTwoSize={animalTwoSize}
-          setAnimalOneSize={setAnimalOneSize}
-          setAnimalTwoSize={setAnimalTwoSize}
-        />
-        <OpenSign 
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-        />
-        <Parade 
-          animals={animals}
-          setAnimals={setAnimals}
-        />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          Welcome to Busy Zoo!
+          <NavLink className={'navHeader'} to={'/'}>Home</NavLink>
+          <NavLink className={'navHeader'} to={'/admin'}>Admin</NavLink>
+        </header>
+        <Switch>
+          <Route exact path={'/'}>
+            <main>
+              <Fight 
+                animalOneSize={animalOneSize}
+                animalTwoSize={animalTwoSize}
+                setAnimalOneSize={setAnimalOneSize}
+                setAnimalTwoSize={setAnimalTwoSize}
+              />
+              <OpenSign 
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+              />
+              <Parade 
+                animals={animals}
+                setAnimals={setAnimals}
+              />
+            </main>
+          </Route>
+          <Route exact path={'/admin'}>
+            <AdminPage />
+          </Route>  
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
